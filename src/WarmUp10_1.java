450f2c4a6a0209e7a96dfdce3fec606752f3f598
kchung1068 <kchung1068@stu.d214.org>
2021-02-17 15:06:43 -0600

    ff

---
+++ src/WarmUp10_1.java
@@ -0,0 +1,24 @@
+/* WarmUp 10_1
+ * Create an integer array of size 4, placing any 4 numbers
+ * you want into the array. Print out the array so that it
+ * looks like [1,5,7,3]. Then increase each element of the 
+ * array by 1 and re-print, [2,6,8,4]
+ */
+
+public class WarmUp10_1 {
+
+	public static void main(String[] args) {
+		int[] array = {1,5,7,3};
+		String list = "";
+
+		for (int x = 0; x < array.length; x += 1) {
+			list += array[x];
+			list += ", ";
+		}
+		System.out.println(list);
+		for (int x = 0; x < array.length; x += 1) {
+			array[x] += 1;
+		}
+		System.out.println(array);
+	}
+}
